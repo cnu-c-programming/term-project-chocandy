94d57ca2255091a3a56a4c76d25d3116e3693e0b
chominjeong <chomj4836@gmail.com>
2026-06-18 09:25:17 +0900

    명령파일 배치실행기능 추가

--- main.c
+++ main.c
@@ -54,9 +54,40 @@ void run_shell(const char *csv_path) {
  *   - Close the file when done.
  * --------------------------------------------------------------- */
 void run_command_file(const char *cmd_file, const char *csv_path) {
-    /* TODO */
-    (void)cmd_file;
-    (void)csv_path;
+    FILE *fp = fopen(cmd_file, "r");
+    if (fp == NULL) {
+        printf("Error: cannot open command file.\n");
+        return;
+    }
+
+    ShellContext ctx;
+    ctx.head = NULL;
+    ctx.csv_path = csv_path;
+
+    load_students_from_csv(csv_path, &ctx.head);
+
+    char line[256];
+    int line_no = 0;
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        line_no++;
+        line[strcspn(line, "\n")] = '\0';
+        /* 빈 줄 무시 */
+        if (line[0] == '\0') {
+            continue;
+        }
+        /* # 주석 무시 */
+        if (line[0] == '#') {
+            continue;
+        }
+        printf("[%d] %s\n", line_no, line);
+        ShellResult result = execute_command(line, &ctx);
+        if (result == SHELL_EXIT) {
+            break;
+        }
+    }
+    fclose(fp);
+    free_students(ctx.head);
 }
 
 int main(int argc, char *argv[]) {
